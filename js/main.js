96ada06157cec7916882228e4324718c93d0f0ae
Lindsey Flores <profe.linaf.1@gmail.com>
2025-02-18 07:48:52 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-// --------- Responsive Navbar Animation Fix -----------
-function test() {
-    var tabsNewAnim = $('#navbarSupportedContent');
-    var activeItemNewAnim = tabsNewAnim.find('.active');
+// --------- Responsive Navbar Animation on Hover -----------
+function test(element) {
+    var activeItemNewAnim = $(element);
 
     if (activeItemNewAnim.length) {
         var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
@@ -20,36 +19,26 @@ function test() {
 
 // Run test function on page load
 $(document).ready(function(){
-    setTimeout(test, 500); // Allow time for styles to load
+    setTimeout(() => test($("#navbarSupportedContent .active")), 500); // Allow time for styles to load
 
-    // Navbar click event listener
-    $("#navbarSupportedContent").on("click", "li", function() {
-        $('#navbarSupportedContent ul li').removeClass("active");
-        $(this).addClass('active');
-        test();
+    // Change animation to hover instead of click
+    $("#navbarSupportedContent").on("mouseenter", "li", function() {
+        test(this);
+    });
+
+    // Adjust animation on window resize
+    $(window).on("resize", function(){
+        setTimeout(() => test($("#navbarSupportedContent .active")), 500);
     });
 });
 
-// Adjust animation on window resize
-$(window).on('resize', function(){
-    setTimeout(test, 5);
-});
-
-
-
-// -------------- Add active class when switching pages ----------
+// -------------- Keep Active Class for Page Switching ----------
 jQuery(document).ready(function($){
-    // Get current path (ignoring query params)
     var path = window.location.href.split("/").pop().split("?")[0];
-
-    // Default to index.html if path is empty
     if (path === '') {
         path = 'index.html';
     }
-
-    // Find corresponding navbar link
     var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
     target.parent().addClass('active');
-    test();
+    test(target.parent());
 });
-
